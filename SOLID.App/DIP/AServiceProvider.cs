26d4f01ee2628c8cc79431b9dc298f306d26be5e
codewithfemi <codewithfemi@gmail.com>
2021-10-11 01:56:35 +0200

    ft-1 added comment for broken A service

--- SOLID.App/DIP/AServiceProvider.cs
+++ SOLID.App/DIP/AServiceProvider.cs
@@ -2,6 +2,8 @@
 
 namespace SOLID.App
 {
+    // TODO: A Service not working as expected,
+    // please fix before the 15th of October 2021
     public class AServiceProvider : ISmsServiceProvider
     {
         public void SendSms(string cellPhone)
