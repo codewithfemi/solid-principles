2245875cc7d3ff6144f39a925836132752ca054d
codewithfemi <codewithfemi@gmail.com>
2021-10-11 02:09:17 +0200

    ft-2 Fixed A Service Provider Issues

--- SOLID.App/DIP/AServiceProvider.cs
+++ SOLID.App/DIP/AServiceProvider.cs
@@ -2,13 +2,10 @@
 
 namespace SOLID.App
 {
-    // TODO: A Service not working as expected,
-    // please fix before the 15th of October 2021
     public class AServiceProvider : ISmsServiceProvider
     {
         public void SendSms(string cellPhone)
         {
-            throw new Exception();
             Console.WriteLine($"Sending SMS to cell phone: {cellPhone}");
         }
     }
